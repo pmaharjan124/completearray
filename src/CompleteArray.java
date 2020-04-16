5cde61a601f33b6c1e7031b82f973fd4177aad30
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-16 18:58:35 -0400

    first commit

---
+++ src/CompleteArray.java
@@ -0,0 +1,22 @@
+
+//Create a variables
+//assign variable with array index value
+//And then add variables
+//Print
+
+
+
+import java.util.Scanner;
+
+public class CompleteArray {
+public static void main(String []args)
+{
+    int[] numbers = {2, 29, 33, 4, 15, 62, 7, 81, 9, 10};
+    int sum = 0;
+    for (int i = 0; i< numbers.length; i++)
+    {
+        sum = sum +numbers[i];
+        System.out.println(sum);
+    }
+}
+ }
